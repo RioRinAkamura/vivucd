cf322bab5deca24001b07d673ef849c34ddd6ea1
RioRinAkamura <nguyenthanhrin26@gmail.com>
2022-04-12 22:49:20 +0700

    add craco

--- src/App.tsx
+++ src/App.tsx
@@ -13,28 +13,31 @@ import Pricing from "./components/Pricing";
 import Contact from "./components/Contact";
 import AppFooter from "./components/common/AppFooter";
 
-const { Header, Content, Footer }: any = Layout;
-
-export function App() {
+const { Header, Content, Footer } = Layout;
+function App() {
   return (
-    <Layout className="mainLayout">
-      <Header>
-        <AppHeader />
-        <div className="logo" />
-      </Header>
-      <Content>
-        <Home />
-        <About />
-        <Features />
-        <Works />
-        <Faq />
-        <Pricing />
-        <Contact />
-      </Content>
-      <Footer>
-        <AppFooter />
-      </Footer>
-    </Layout>
+    <>
+      <Layout className="mainLayout">
+        <Header>
+          <AppHeader />
+          <div className="logo" />
+        </Header>
+        <Content>
+          <Home />
+          <About />
+          <Features />
+          <Works />
+          <Faq />
+          <Pricing />
+          <Contact />
+        </Content>
+        <Footer>
+          <AppFooter />
+        </Footer>
+      </Layout>
+    </>
   );
 }
+
+export default App;
 
